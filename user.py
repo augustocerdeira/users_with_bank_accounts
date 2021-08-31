c3ea52c64cd1bafbb575144903dd3200f81fe746
Augusto Cerdeira <augusto.borbi@gmail.com>
2021-08-31 16:03:33 -0700

    User_chaining

--- user.py
+++ user.py
@@ -5,26 +5,25 @@ class User:
         self.account_balance = 0
     def make_deposit(self, amount):	
         self.account_balance += amount	
+        return self
     def makeWithdrawal(self,amount):
         self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print(self.name +  " Balance $", self.account_balance)
+        return self
     def tranfer_money(self,other_user,amount):
         self.account_balance -= amount
         other_user.account_balance += amount
         print(other_user.account_balance)
+        return self
 
 
 guido = User("Guido", "guido@mail.com")
 monty = User("Monty", "monty@mail.com")
-guido.make_deposit(300)
-monty.make_deposit(200)
+guido.make_deposit(300).makeWithdrawal(150).display_user_balance().tranfer_money(monty, 25)
+monty.make_deposit(200).makeWithdrawal(50)
 print(guido.account_balance)
 print(monty.account_balance)
-guido.makeWithdrawal(150)
-monty.makeWithdrawal(50)
-print(guido.account_balance)
-print(monty.account_balance)
-guido.display_user_balance()
-guido.tranfer_money(monty, 25)
+
 
